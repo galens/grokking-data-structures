12a4a955c7613521861b2c8b6f030571bf74f01c
Galen <galen.senogles@gmail.com>
2024-11-21 14:20:19 -0700

    next-greater-element

---
+++ next-greater-element.js
@@ -0,0 +1,29 @@
+class Solution {
+  static nextLargerElement(arr) {
+    const stack = [];
+    const res = new Array(arr.length).fill(0);
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+      while (stack.length && stack[stack.length - 1] <= arr[i]) {
+        // While the stack is not empty and the element at the top of the stack
+        // is less than or equal to the current element, pop elements from the stack
+        stack.pop();
+      }
+
+      if (stack.length) {
+        // If the stack is not empty after popping, it means the current element
+        // has found its next larger element
+        res[i] = stack[stack.length - 1];
+      }
+
+      // Push the current element's index onto the stack
+      stack.push(arr[i]);
+    }
+    console.log('res: ', res);
+  }
+}
+
+console.log(Solution.nextLargerElement([13, 7, 6, 12]));
+
+// time complexity O(N)
+// space complexity O(N)

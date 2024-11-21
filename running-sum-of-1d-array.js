c27a506a3f22d3cc7227c175b6952c1cfc742c1b
Galen <galen.senogles@gmail.com>
2024-11-20 18:56:52 -0700

    running sum solution

--- running-sum-of-1d-array.js
+++ running-sum-of-1d-array.js
@@ -1,1 +1,12 @@
-console.log('test');
+class Solution {
+  static runningSum(nums) {
+    const result = [];
+    result.push(nums[0]);
+    for (let i = 1; i < nums.length; i++) {
+      result.push(nums[i] + result[i - 1]);
+    }
+    return result;
+  }
+}
+
+console.log(Solution.runningSum([2, 3, 5, 1, 6]));

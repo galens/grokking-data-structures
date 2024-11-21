3f66319ffff6f8103a55beddb2a4995c40cc5e33
Galen <galen.senogles@gmail.com>
2024-11-21 11:26:55 -0700

    add balanced parentheses

---
+++ balanced-parentheses.js
@@ -0,0 +1,33 @@
+class Solution {
+  static isValid(s) {
+    let characterStack = [];
+    characterStack.push(s[0]);
+    for (let i = 1; i < s.length; i++) {
+      if (s[i] === '{' || s[i] === '[' || s[i] === '(') {
+        characterStack.push(s[i]);
+      } else {
+        if (s[i] === '}' && characterStack[characterStack.length - 1] === '{') {
+          characterStack.pop();
+        } else if (
+          s[i] === ')' &&
+          characterStack[characterStack.length - 1] === '('
+        ) {
+          characterStack.pop();
+        } else if (
+          s[i] === ']' &&
+          characterStack[characterStack.length - 1] === '['
+        ) {
+          characterStack.pop();
+        } else {
+          characterStack.push(s[i]);
+        }
+      }
+    }
+    if (!characterStack.length) {
+      return true;
+    }
+    return false;
+  }
+}
+
+console.log(Solution.isValid('{[()]]}'));
